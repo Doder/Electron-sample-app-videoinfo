3fabf4b7a0d65b9eb3f1ebe3ccaf5ab72dad4344
Aleksa <aleksadoderovic@gmail.com>
2018-02-03 17:09:15 +0100

    add tray icon, show window on click and position it under tray icon, add tool tip, add quit option on right click

--- tasky/index.js
+++ tasky/index.js
@@ -1,17 +1,22 @@
 const path = require('path');
 const electron = require('electron');
-const {app, BrowserWindow, Tray} = electron;
+const {app, BrowserWindow} = electron;
+const TimerTray = require('./app/timer_tray');
 let mainWindow;
+
+let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     height: 500,
     width: 300,
     frame: false,
-    resizable: false
+    resizable: false,
+    show: false
   });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconName = process.platform === 'win32' ||
+  process.platform === 'linux' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  new Tray(iconPath);
+  tray = new TimerTray(iconPath, mainWindow);
 });
